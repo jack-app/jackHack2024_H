b23f0d6cb5bbd4629175ab92fc44e72771a2155c
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-05-04 20:55:49 +0900

    wip

--- backend/google_api_token_getter/main.py
+++ backend/google_api_token_getter/main.py
@@ -1,37 +1,36 @@
-import requests
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 
+_auth_flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+    './credential.json',
+    scopes=['https://www.googleapis.com/auth/calendar.events']
+)
+_auth_flow.redirect_uri = 'https://jack.hbenpitsu.net/authRedirect'
 
 class GoogleApiTokenGetter:
-    # def __init__(self, client_id: str, client_secret: str, redirect_uri: str):
-    #     self.client_id = client_id
-    #     self.client_secret = client_secret
-    #     self.redirect_uri = redirect_uri
+    unsinged_states = {}
+    signed_states = {}
 
-    def __init__(self):
-        self.client_id = "client_id"
-        self.client_secret = "client_secret"
-        self.redirect_uri = "redirect_uri"
+    def sing(state: str, code: str):
+        """
+        stateに対応するcodeを受け取り、stateを削除して、codeをstateに対応するsigned_statesに追加する。
+        これに失敗した場合はValueErrorを返す。
+        """
+        if state in GoogleApiTokenGetter.unsigned_states:
+            GoogleApiTokenGetter.signed_states[state] = code
+            del GoogleApiTokenGetter.unsigned_states[state]
+            return
+        else:
+            raise ValueError(
+                f"designated state: '{state}' is not found in unsigned_states: {GoogleApiTokenGetter.unsigned_states.keys()}"
+            )
 
-    def get_token(self, code: str) -> str:
-        url = "https://oauth2.googleapis.com/token"
-        data = {
-            "code": code,
-            "redirect_uri": self.redirect_uri,
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-            "grant_type": "authorization_code",
-        }
-        response = requests.post(
-            url,
-            data=data,
-            headers={
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        )
-        return response.json()
+    # async def pop_token(self, timeout) -> str:
 
     def get_oauth_url(self) -> str:
-        # Get OAuth URL
-        return "https://accounts.google.com/o/oauth2/auth?client_id={}&redirect_uri={}&response_type=code&scope=https://www.googleapis.com/auth/drive".format(
-            self.client_id, self.redirect_uri
+        authorization_url, state = self.auth_flow.authorization_url(
+            accsess_type='offline',
+            include_granted_scopes='true'
         )
+        GoogleApiTokenGetter.sessionToken_stateMap[state] = self
+        return authorization_url
