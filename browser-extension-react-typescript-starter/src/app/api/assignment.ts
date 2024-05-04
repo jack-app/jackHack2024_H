fb263f914a4aff0ca91e2527b7ce1cf983d2078b
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-05-04 23:52:31 +0900

    api wrapper移植

---
+++ browser-extension-react-typescript-starter/src/app/api/assignment.ts
@@ -0,0 +1,42 @@
+import CourseEntry, { getFavoriteCoursesIds, getRawCourse } from './course';
+import { fetcher } from './fetcher';
+import TaskEntry from './task';
+
+// TODO: 型を追加する
+export const getRawAllAssignments = async (): Promise<any> => {
+  const data = await fetcher.fetch('/direct/assignment/my.json');
+  if (data['assignment_collection'] === undefined) {
+    return [];
+  }
+  return data['assignment_collection'];
+};
+
+export const getAssignments = async (): Promise<TaskEntry[]> => {
+  const rawAssignments = await getRawAllAssignments();
+  const favoriteCourses = await getFavoriteCoursesIds();
+
+  const courses = await Promise.all(
+    favoriteCourses.map(async (courseId: string) => {
+      const course = await getRawCourse(courseId);
+      console.log('course', course);
+      return new CourseEntry(course['entityId'], course['entityTitle']);
+    })
+  );
+
+  // TODO: 型を追加する
+  const assignments = rawAssignments.map((assignment: any) => {
+    let course = courses.find((course) => course.id === assignment['context']);
+    if (course === undefined) {
+      course = new CourseEntry('unknown', 'unknown');
+    }
+    const tasks = new TaskEntry(
+      assignment['entityId'],
+      assignment['entityTitle'],
+      assignment['dueTimeString'],
+      course.name,
+      course.id
+    );
+    return tasks;
+  });
+  return assignments;
+};
