67f5a317fbedcc8c1eb555e12fd64d19a9de0d37
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-05-05 00:20:14 +0900

    見当違いのコメント(TODO: 非同期化)を削除

--- backend/google_api_token_getter/main.py
+++ backend/google_api_token_getter/main.py
@@ -55,7 +55,6 @@ class GoogleApiTokenGetter:
 
         return GAPITokenBundle(tokens["access_token"], tokens["refresh_token"],tokens["expires_at"] )
 
-    # TODO: 非同期化する
     def get_oauth_url(self) -> str:
         authorization_url, state = AUTH_FLOW.authorization_url(
             accsess_type='offline',
