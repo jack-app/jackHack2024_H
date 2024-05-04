db313333e6d3bf2653ea9b7192515984c2d4aad0
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-05-04 13:51:25 +0900

    totyuu

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,28 @@
-from fastapi import FastAPI, Request
-from .calenderapiwrapper.main import CalendarAPIWrapper
+from fastapi import FastAPI, Request, Response
+from calenderapiwrapper.main import CalendarAPIWrapper
 
 app = FastAPI()
 
 @app.get("/")
-def rootRoute():
+def rootRoute(response:Response, request:Request):
+    print(response)
+    print(request)
     return "You're successfully accessing to the FastAPI server."
 
-@app.get("/register")
-def register_entry(request: Request):
-    pass
+@app.get("/register/")
+def register_entry(response:Response, request:Request):
+    # (header)cookieでsessionTokenを受付
+    # (body)jsonでassignmentEntryを受付
+    response.status_code = 200
+    # sessionToken = request.headers[""]
+    print(request.headers)
+    assignmentEntry = request.body
+    return response
 
-@app.get("/getToken")
-def get_token(request: Request):
-    pass
+@app.get("/getToken/")
+def get_token(response:Response, request:Request):
+    # response.set_cookie(key="test", value="test")
+    return response
 
 if __name__ == "__main__":
     import uvicorn
