ed38f56c17a708b0de8c622ca0c6f2ac6d767be8
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-05-04 15:48:54 +0900

    register_entryとget_tokenがかけました(API部分)

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 from fastapi import Body, FastAPI, Request, Response
 from pydantic import BaseModel, Field
 from shared.AssignmentEntry import AssignmentEntry
+from assignmentEntryRegister import assignmentEntryRegister
 from calenderapiwrapper.main import CalendarAPIWrapper
+from sessionmanager import SessionManager
 
 app = FastAPI()
+
 
 @app.get("/")
 def rootRoute(response:Response, request:Request):
@@ -19,19 +22,30 @@ async def register_entry(body: AssignmentEntry,response:Response, request:Reques
     # fetch("http://127.0.0.1:61000/bodyGetTest",
     #   {method:"POST",headers: {
     #       'Content-Type': 'application/json',
-    #       'sessionToken': 
+    #       'sessionToken': 'token'
     #   },
     #   body:JSON.stringify(assignmentEntry)}
     # ) のようにしてリクエストを送ってください。
 
-    
-
-    return body
+    try:
+        assignmentEntryRegister(body,request.headers["sessionToken"])
+        response.status_code = 200
+        response.body = {}
+    except Exception as error:
+        response.status_code = 500
+        response.body = {
+            "msg":str(error)
+        }
 
 
 @app.get("/getToken")
 async def get_token(response:Response, request:Request):
-    # response.set_cookie(key="test", value="test")
+    sessionManager = SessionManager()
+    response.status_code = 200
+    response.set_cookie(key="sessionToken", value=sessionManager.getSessionToken(),secure=True)
+    response.body = {
+        "auth_url": sessionManager.getAuthURL()
+    }
     return response
 
 if __name__ == "__main__":
