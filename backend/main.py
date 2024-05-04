1935461ae02f69556d3e7b33ecd70aa3d480121d
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-05-04 15:23:51 +0900

    bodyを受け取り

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,9 @@
-from fastapi import FastAPI, Request, Response
-from pydantic import BaseModel
+from fastapi import Body, FastAPI, Request, Response
+from pydantic import BaseModel, Field
 from shared.AssignmentEntry import AssignmentEntry
 from calenderapiwrapper.main import CalendarAPIWrapper
 
 app = FastAPI()
-
-from pydantic import BaseModel
-from fastapi import FastAPI, Request, status
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-@app.exception_handler(RequestValidationError)
-async def handler(request:Request, exc:RequestValidationError):
-    print(exc)
-    return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
-
 
 @app.get("/")
 def rootRoute(response:Response, request:Request):
@@ -21,27 +11,15 @@ def rootRoute(response:Response, request:Request):
     print(request)
     return "You're successfully accessing to the FastAPI server."
 
-from typing import Union
-class TestBody(BaseModel):
-    test: Union [str , None] = None
-@app.post("/register/")
-def register_entry(body: TestBody ): #,response:Response, request:Request
+@app.post("/register")
+async def register_entry(body: AssignmentEntry,response:Response, request:Request):
     # (header)cookieでsessionTokenを受付
     # (body)jsonでassignmentEntryを受付
     print(body)
 
-    # response.status_code = 200
-    # # sessionToken = request.headers[""]
-    # print(request.headers)
-    # assignmentEntry = request.body
-    # return response
-    # print(request.body())
-    # response.status_code = 200
-    # response.body = {}
-    # return response
 
-@app.get("/getToken/")
-def get_token(response:Response, request:Request):
+@app.get("/getToken")
+async def get_token(response:Response, request:Request):
     # response.set_cookie(key="test", value="test")
     return response
 
