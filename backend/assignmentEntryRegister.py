280c45d9d95340a8ecaa025877e07e3d125692e0
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-05-04 15:31:15 +0900

    コメントを入れました

--- backend/assignmentEntryRegister.py
+++ backend/assignmentEntryRegister.py
@@ -1,4 +1,5 @@
 from shared.AssignmentEntry import AssignmentEntry
 
-def assignmentEntryRegister(assignmentEntry: AssignmentEntry):
+# 追加に成功したらTrueを返してください。そうでなければFalse
+async def assignmentEntryRegister(assignmentEntry: AssignmentEntry)->bool:
     pass
