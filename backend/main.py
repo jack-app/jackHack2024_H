e8c0a0cc004a3639ab52d6642b930e3d55c97696
nagoyayuu15 <nagoyayuu15@gmail.com>
2024-05-04 12:11:08 +0900

    ルーティングを追加

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from .calenderapiwrapper.main import CalendarAPIWrapper
 
 app = FastAPI()
 
 @app.get("/")
 def rootRoute():
     return "You're successfully accessing to the FastAPI server."
+
+@app.get("/register")
+def register_entry(request: Request):
+    pass
+
+@app.get("/getToken")
+def get_token(request: Request):
+    pass
 
 if __name__ == "__main__":
     import uvicorn
