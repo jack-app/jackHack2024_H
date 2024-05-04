6f5255e2467df3aac1aeb281f8699868e6f58e27
xFonce <hattori.kazma@gmail.com>
2024-05-04 14:17:21 +0900

    a

--- backend/sessionmanager/main.py
+++ backend/sessionmanager/main.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class SessionManager:
     def __init__(self) -> None:
         pass
@@ -11,4 +14,30 @@ class SessionManager:
     def getGoogleAPIToken(self, sessionToken):
         # if sessionToken in db return apitoken
         # else create apitoken
-        pass
+
+        dbname = 'test.db'
+        conn = sqlite3.connect(dbname)
+        c = conn.cursor()
+
+        line = c.execute(
+            f"select actoken from users where sstoken=?", [sessionToken]).fetchone()
+
+        if line is None:
+            print("data not found")
+            # create actoken
+            # add actoken sstoken
+            # return apitoken
+
+        accessToken = line[0]
+        if accessToken is None:
+            # create actoken
+            # add sstoken
+            # return actoken
+            pass
+
+        return accessToken
+
+
+if __name__ == "__main__":
+    sm = SessionManager()
+    sm.getGoogleAPIToken("ss1")
--- backend/sessionmanager/test.py
+++ backend/sessionmanager/test.py
@@ -3,4 +3,14 @@ import sqlite3
 dbname = 'db/test.db'
 conn = sqlite3.connect(dbname)
 
+
+cur = conn.cursor()
+cur.execute(
+    'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT,token STRING)')
+
+# データベースへコミット。これで変更が反映される。
+conn.commit()
+
+print(cur.fetchall())
+
 conn.close()
