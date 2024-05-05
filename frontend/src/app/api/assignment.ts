9ad65f4fbcc7e3187ce6d56c38c41e086bf4cc01
浜田　俊祐 <hamashun0820@icloud.com>
2024-05-05 17:28:18 +0900

    add: openTime

--- frontend/src/app/api/assignment.ts
+++ frontend/src/app/api/assignment.ts
@@ -29,9 +29,11 @@ export const getAssignments = async (): Promise<TaskEntry[]> => {
     if (course === undefined) {
       course = new CourseEntry('unknown', 'unknown');
     }
+    // TODO: Date型に変換する
     const tasks = new TaskEntry(
       assignment['entityId'],
       assignment['entityTitle'],
+      assignment['openTimeString'],
       assignment['dueTimeString'],
       course.name,
       course.id
