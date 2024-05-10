58f346cf6f4bf098e92ef1b2c2f0ab96bdf01690
hbenpitsu <hbenpitsu15@gmail.com>
2024-05-10 16:28:55 +0900

    mainで作業してたことに今気づいた。最悪や エラーハンドラー内部の名前をかえた

--- backend/shared/errorHandler.py
+++ backend/shared/errorHandler.py
@@ -1,9 +1,9 @@
 from typing import Callable,Any
 from fastapi import Response
 
-def handlerErrorAsHTTPResponse(trial:Callable[[],Any], response:Response):
+def handlerErrorAsHTTPResponse(closure:Callable[[],Any], response:Response):
     try:
-        return trial()
+        return closure()
     except Exception as e:
         if hasattr(e, "http_status"):
             response.status_code = e.http_status
