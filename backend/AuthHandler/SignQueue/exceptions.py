4c90b2860ae76395a2a4b5b43f43414724a86dde
hbenpitsu <hbenpitsu15@gmail.com>
2024-05-11 23:55:38 +0900

    exceptionsを編集

--- backend/AuthHandler/SignQueue/exceptions.py
+++ backend/AuthHandler/SignQueue/exceptions.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 class StateNotExists(HTTPException):
-    def __init__(self, state:str):
+    def __init__(self, state:str|None=None):
         super().__init__(status_code=400, detail=f"State {state} does not exist.")
 class TokenFechingTimeout(HTTPException):
-    def __init__(self, msg:str):
+    def __init__(self, msg:str|None=None):
         super().__init__(status_code=408, detail=msg or "Token fetching timeout.")
